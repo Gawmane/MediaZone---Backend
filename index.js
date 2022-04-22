f930c231a2569e4e0f4a74078c73d963d1a24aaf
Hydrille <03rytter@gmail.com>
2022-04-22 09:30:16 +0200

    fix calling from web

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import EventRouter from './Routes/user.router.js';
-import UserRouter from './Routes/event.router.js';
+import express from "express";
+import EventRouter from "./Routes/user.router.js";
+import UserRouter from "./Routes/event.router.js";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 //Kalder en environment vars
 dotenv.config();
@@ -11,16 +11,41 @@ const port = process.env.PORT || 4000;
 
 //Laver en json
 const app = express();
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+	express.urlencoded({
+		extended: true,
+	}),
+	express.json(),
+	function (req, res, next) {
+		// Website you wish to allow to connect
+		res.setHeader("Access-Control-Allow-Origin", "*");
+
+		// Request methods you wish to allow
+		res.setHeader(
+			"Access-Control-Allow-Methods",
+			"GET, POST, OPTIONS, PUT, PATCH, DELETE"
+		);
+
+		// Request headers you wish to allow
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"X-Requested-With,content-type"
+		);
+
+		// Set to true if you need the website to include cookies in the requests sent
+		// to the API (e.g. in case you use sessions)
+		res.setHeader("Access-Control-Allow-Credentials", true);
+
+		// Pass to next layer of middleware
+		next();
+	}
+);
 
 //Brug router
 app.use(EventRouter);
 app.use(UserRouter);
 
-
-//Kalder serveren 
+//Kalder serveren
 app.listen(port, () => {
-    console.log(`Server køre på port http://localhost:${port}`);
-})
+	console.log(`Server køre på port http://localhost:${port}`);
+});
