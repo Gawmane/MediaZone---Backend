0b2ff164e87eeb19a234b12cc4ce812e870db8bc
mikk7021 <mikk7021@gmail.com>
2022-04-22 11:05:35 +0200

    mege

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,85 +1,62 @@
 import UserModel from "../Models/user.model.js";
 
 class UserController {
-	constructor() {}
+    constructor() {}
 
-	//Henter list
-	list = async (req, res) => {
-		const result = await UserModel.findAll({
-			//limit: 2,
-			order: ["firstname"],
-		});
-		res.status(200).json({ status: "OK", data: result });
-	};
+    //Henter list
+    list = async(req, res) => {
+        const result = await UserModel.findAll({
+            //limit: 2,
+            order: ['firstname']
+        });
+        res.send(result);
+    }
 
-	//Henter details
-	get = async (req, res) => {
-		try {
-			const result = await UserModel.findAll({ where: { id: req.params.id } });
-			return res.status(200).json({ status: "OK", data: result });
-		} catch (err) {
-			return res.status(400).json({ status: "ERROR", error: err });
-		}
-	};
+    //Henter details
+    get = async(req, res) => {
+        try {
+            const result = await UserModel.findAll({ where: { id: req.params.id } });
+            return res.json(result);
+        } catch (err) {
+            return res.send(err);
+        }
+    }
 
-	//Opret en user
-	create = async (req, res) => {
-		//Fra database - skal være der for at oprette et eventt
-		const { firstname, lastname, email, password, class_id } = req.body;
+    //Opret en user 
+    create = async(req, res) => {
+        //Fra database - skal være der for at oprette et eventt
+        const { firstname, lastname, email, password, class_id } = req.body;
+        console.log(req.body)
+        if (firstname && lastname && email && password && class_id) {
+            const model = await UserModel.create(req.body);
+            return res.status(200).json({ newid: model.id });
+        } else {
+            const errMsg = "Something went wrong in POST Create user"
+            return res.send(errMsg)
+        }
+    }
 
-		const errors = {};
-		if (!firstname) errors["firstname"] = "string";
-		if (!lastname) errors["lastname"] = "string";
-		if (!email) errors["email"] = "string";
-		if (!password) errors["password"] = "string";
-		if (!class_id) errors["class_id"] = "int";
+    //Opdater en user 
+    update = async(req, res) => {
+        const { firstname, lastname, email, password, id } = req.body;
 
-		if (firstname && lastname && email && password && class_id) {
-			const model = await UserModel.create(req.body);
-			return res.status(201).json({ status: "OK", id: model.id });
-		} else {
-			return res.status(400).json({
-				status: "ERROR",
-				missing: errors,
-			});
-		}
-	};
+        if (firstname && lastname && email && password) {
+            await UserModel.update(req.body, { where: { id: id } });
+            return res.sendStatus(200);
+        } else {
+            return res.send(418)
+        }
+    }
 
-	//Opdater en user
-	update = async (req, res) => {
-		const { firstname, lastname, email, password, id, class_id } = req.body;
-
-		const errors = {};
-		if (!firstname) errors["firstname"] = "string";
-		if (!lastname) errors["lastname"] = "string";
-		if (!email) errors["email"] = "string";
-		if (!password) errors["password"] = "string";
-		if (!class_id) errors["class_id"] = "int";
-		if (!id) errors["id"] = "int";
-
-		if (firstname && lastname && email && password) {
-			await UserModel.update(req.body, { where: { id: id } });
-			return res.status(200).json({ status: "OK", updated_id: id });
-		} else {
-			return res.status(400).json({
-				status: "ERROR",
-				missing: errors,
-			});
-		}
-	};
-
-	//Slet en user
-	delete = async (req, res) => {
-		try {
-			await UserModel.destroy({ where: { id: req.params.id } });
-			res.status(200).json({ status: "OK", deleted_id: req.params.id });
-		} catch (err) {
-			return res.status(400).json({
-				status: "ERROR",
-				missing: { id: "int" },
-			});
-		}
-	};
+    //Slet en user
+    delete = async(req, res) => {
+        try {
+            await UserModel.destroy({ where: { id: req.params.id } });
+            res.sendStatus(200)
+        } catch (err) {
+            res.send(err)
+        }
+    }
 }
 
 export default UserController;
--- index.js
+++ index.js
@@ -1,44 +1,83 @@
-import express from "express";
-import EventRouter from "./Routes/user.router.js";
-import UserRouter from "./Routes/event.router.js";
+import express from 'express';
+import EventRouter from './Routes/user.router.js';
+import UserRouter from './Routes/event.router.js';
+import cors from "cors";
+
+
 
 import dotenv from "dotenv";
 
+
+
 //Kalder en environment vars
+
 dotenv.config();
+
+
 
 const port = process.env.PORT || 4000;
 
-//Laver en json
+
+
 const app = express();
+
+
 app.use(
-	express.urlencoded({
-		extended: true,
-	}),
-	express.json(),
-	function (req, res, next) {
-		// Website you wish to allow to connect
-		res.setHeader("Access-Control-Allow-Origin", "*");
 
-		// Request methods you wish to allow
-		res.setHeader(
-			"Access-Control-Allow-Methods",
-			"GET, POST, OPTIONS, PUT, PATCH, DELETE"
-		);
+    express.urlencoded({
 
-		// Request headers you wish to allow
-		res.setHeader(
-			"Access-Control-Allow-Headers",
-			"X-Requested-With,content-type"
-		);
+        extended: true,
 
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		res.setHeader("Access-Control-Allow-Credentials", true);
+    }),
 
-		// Pass to next layer of middleware
-		next();
-	}
+    express.json(),
+
+    function (req, res, next) {
+
+        // Website you wish to allow to connect
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+
+
+        // Request methods you wish to allow
+
+        res.setHeader(
+
+            "Access-Control-Allow-Methods",
+
+            "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+
+        );
+
+
+
+        // Request headers you wish to allow
+
+        res.setHeader(
+
+            "Access-Control-Allow-Headers",
+
+            "X-Requested-With,content-type"
+
+        );
+
+
+
+        // Set to true if you need the website to include cookies in the requests sent
+
+        // to the API (e.g. in case you use sessions)
+
+        res.setHeader("Access-Control-Allow-Credentials", true);
+
+
+
+        // Pass to next layer of middleware
+
+        next();
+
+    }
+
 );
 
 //Brug router
